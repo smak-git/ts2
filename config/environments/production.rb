54c267e7919ea9f8972f9c51fe05c553449e4e7d
smak-git <saurabhsmak@gmail.com>
2014-09-01 12:36:21 +0530

    Add SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Force all access tot he app over SSL, use Strict-Trnasport-Security and use secure cookies
+  config.force_ssl = true
 end
